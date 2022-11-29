c74e11ebd7d2fc4be611caba46fb7572479ab99e
Richie1802 <118230207+Richie1802@users.noreply.github.com>
2022-11-29 13:08:36 -0500

    completed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener('load', function(){
+   const fetchData = async() =>{
+    const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json'); 
+    const data = await response.json();
+        console.log(data);
+        
+            const container = document.getElementById("container");
+            let astronautData= "";
+            for (let astronaut of data) {
+                astronautData =+ `<div class = "astronaut>
+                <div class = "bio".
+                <h3>${astronaut.firstName} ${astronaut.lastName}<h3>
+                <h5> Hours In Space: ${astronaut.HoursInSpace}<h5>
+                <h5> Active: ${astronaut.Active}<h5>
+                <h5>Skills: ${astronaut.skills.join (',')}<h5>
+                </div> <img class = "avatar" src= "${astronaut.picture}">
+                </div>`;
+            }
+            container.innerHTML += astronautData;
+        }
+        fetchData();
+        });
+    
